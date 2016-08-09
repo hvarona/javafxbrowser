df9c900811bf1d6c9756c3fec7424d539a23d9ab
Henry Varona <>
2016-08-09 14:16:46 -0400

    Fiurst Version

---
+++ src/javafxbrowser/JavaFxBrowser.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxbrowser;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafxbrowser.frame.BrowserFrame;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author henry
+ */
+public class JavaFxBrowser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+                } catch (Exception e) {
+                }
+                
+                BrowserFrame main = new BrowserFrame();
+                main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                main.setVisible(true);
+                main.loadURL("http://www.google.com/"); //Home Page
+            }
+        });
+    }
+}
